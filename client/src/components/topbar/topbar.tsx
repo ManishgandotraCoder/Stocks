09d2a1512d477e730676b65984ae14c76a8aa855
manish gandotra <manishgandotra@icloud.com>
2023-03-20 14:00:57 +0530

    changes

--- client/src/components/topbar/topbar.tsx
+++ client/src/components/topbar/topbar.tsx
@@ -1,4 +1,4 @@
-import { Toolbar, Box, Popover, Typography, Menu, MenuItem } from "@mui/material"
+import { Toolbar, Box, Switch , Typography, Menu, MenuItem, FormControlLabel, Button, AppBar } from "@mui/material"
 import React from "react";
 import AppIcon from "../../images/icon2.png"
 import "./topbar.scss"
@@ -7,9 +7,8 @@ import FiberNewIcon from '@mui/icons-material/FiberNew';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useNavigate } from "react-router-dom";
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ContentCopy from '@mui/icons-material/ContentCopy';
 import ListItemText from '@mui/material/ListItemText';
-
+import DarkModeTwoToneIcon from '@mui/icons-material/DarkModeTwoTone';
 const Header = () => {
   const navigate = useNavigate()
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
@@ -23,43 +22,46 @@ const Header = () => {
   };
 
   const open = Boolean(anchorEl);
+  const label = { inputProps: { 'aria-label': 'Color switch demo' } };
 
   return <>
-    <Box sx={{ flexGrow: 1 }} className="toolbar">
-      <Toolbar style={{ background: '#FFF' }} variant="dense">
-        <img className="img" src={AppIcon} />
-        <Typography className="dms" onClick={() => navigate('/')}>Capital $tocks</Typography>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar className="toolbar">
+        <Toolbar style={{ background: '#000' }} variant="dense" >
+          <img className="img" src={AppIcon} />
+          <Typography className="dms" onClick={() => navigate('/')}>Capital $tocks</Typography>
 
-        <span onMouseOver={handleClick} className="icon_text">Discover</span>  <ArrowDropDownIcon className="icon_text2" />
+          <span onMouseOver={handleClick} className="icon_text">Discover</span>  <ArrowDropDownIcon className="icon_text2" />
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={() => navigate('/crypto')}>
+              <ListItemIcon>
+                <StorefrontIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Market</ListItemText>
 
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          <MenuItem onClick={() => navigate('/crypto')}>
-            <ListItemIcon>
-              <StorefrontIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>Market</ListItemText>
-
-          </MenuItem>
-          <MenuItem>
-            <ListItemIcon>
-              <FiberNewIcon fontSize="small" />
-            </ListItemIcon>
-            <ListItemText>New Currency</ListItemText>
-          </MenuItem>
-        </Menu>
-        <Box style ={{float :"right"}}>
-          <span>Button Aligned To The Right</span>
-        </Box>
-      </Toolbar>
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>
+                <FiberNewIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>New Currency</ListItemText>
+            </MenuItem>
+          </Menu>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          </Typography>
+          <DarkModeTwoToneIcon/>
+        </Toolbar>
+      </AppBar>
     </Box>
+    
 
   </>
 }
