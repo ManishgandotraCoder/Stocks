2c64566d63528b5ece0fa070727a6afea08ba1a7
manish gandotra <manishgandotra@icloud.com>
2023-03-28 13:18:32 +0530

    path

--- client/src/redux/apis/crypto.api.ts
+++ client/src/redux/apis/crypto.api.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
-const base_url = "http://currencyflow.in:9000/api"
+import base_url from "../../constants/path"
 
 function getData(currency: string) {
     try {
-        return axios.get(`${base_url}/crypto/getCrypto/${currency}`)
+        return axios.get(`${base_url}crypto/getCrypto/${currency}`)
     }
     catch { 
         return false
@@ -12,7 +12,7 @@ function getData(currency: string) {
 
 function getCoinGraph(coin: string, currency: string, days: Number) {
     try {
-        return axios.post(`${base_url}/crypto/getCryptoGraph`, {
+        return axios.post(`${base_url}crypto/getCryptoGraph`, {
             coin: coin,
             days: days,
             currency: currency
@@ -24,7 +24,7 @@ function getCoinGraph(coin: string, currency: string, days: Number) {
 }
 function getCoinDetails(coin: string) {
     try {
-        return axios.get(`${base_url}/crypto/getCryptoDetails/${coin}`)
+        return axios.get(`${base_url}crypto/getCryptoDetails/${coin}`)
     }
     catch {
         return false
